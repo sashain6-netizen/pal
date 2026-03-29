f1ca7c7c3ed04754b6390035b6984cbf0ceef0c0
Simon Shain <sashain6@gmail.com>
2026-03-28 20:46:39 -0700

    update

--- public/users/script.js
+++ public/users/script.js
@@ -39,7 +39,7 @@ function renderAccessoryElement(container, accessory, category, accessoryKey) {
 
 function getColoredSvg(color) {
     return `
-        <svg viewBox="0 0 24 24" fill="${color}" style="width: 100%; height: 100%;">
+        <svg viewBox="0 0 24 24" fill="${color}" style="width: calc(100% - 8px); height: calc(100% - 8px);">
             <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/>
         </svg>`;
 }
