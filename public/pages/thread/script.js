0d1bd310c7aa6d715c03f43386cc4a45dea2d413
Simon Shain <sashain6@gmail.com>
2026-03-28 21:22:11 -0700

    update

--- public/pages/thread/script.js
+++ public/pages/thread/script.js
@@ -9,13 +9,20 @@ function generateAvatar(avatarUrl, userColor, username) {
     if (avatarUrl && avatarUrl !== "" && avatarUrl !== "/default-avatar.png") {
         return `<img src="${avatarUrl}" alt="${username}" class="post-avatar">`;
     } else {
-        return `
-            <div class="post-avatar post-avatar-svg">
-                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="width:100%; height:100%;">
-                    <path d="M12 12C14.21 12 16 10.21 16 8C16 5.79 14.21 4 12 4C9.79 4 8 5.79 8 8C8 10.21 9.79 12 12 12ZM12 14C9.33 14 4 15.34 4 18V20H20V18C20 15.34 14.67 14 12 14Z"
-                          fill="${userColor}" />
-                </svg>
-            </div>`;
+        if (typeof getCircleFillingAvatarSvg === 'function') {
+            return `
+                <div class="post-avatar post-avatar-svg">
+                    ${getCircleFillingAvatarSvg(userColor, '100%')}
+                </div>`;
+        } else {
+            return `
+                <div class="post-avatar post-avatar-svg">
+                    <svg viewBox="0 0 100 100" fill="${userColor}" style="width:100%; height:100%; max-width:100%; max-height:100%;">
+                        <circle cx="50" cy="35" r="18"/>
+                        <path d="M 50 58 C 35 58, 20 65, 15 80 L 15 95 L 85 95 L 85 80 C 80 65, 65 58, 50 58 Z"/>
+                    </svg>
+                </div>`;
+        }
     }
 }
 
